c6591bc6a92dcb165376edc4b13c35625733a02b
ianthomCO <ian.b.thom@gmail.com>
2023-03-15 22:21:07 -0600

    Add contact page initial setup

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,15 +10,21 @@ function loadHeader(){
     const moto = document.getElementById('moto');
 
     title.textContent = "Contact Us";
-    moto.textContent = "How To Get In Touch";
+    moto.textContent = "Get In Touch With Us";
+}
+
+function createMain(){
+
+    return
 }
 
 function loadMain(){
     const main = document.querySelector('main');
-
-
-
+    
+    main.textContent = "";
+    main.appendChild(createMain());  
 }
+
 
 
 export default loadContact;
