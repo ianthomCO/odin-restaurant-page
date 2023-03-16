d33712a63a67a5e7214f17546999ca442cea43bb
ianthomCO <ian.b.thom@gmail.com>
2023-03-15 23:09:16 -0600

    Update contact page and formatting

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,7 +15,30 @@ function loadHeader(){
 
 function createMain(){
 
-    return
+    const contactContainer = document.createElement('div');
+    const street = document.createElement('div');
+    const city = document.createElement('div');
+    const phone = document.createElement('div');
+    const email = document.createElement('div');
+
+    contactContainer.id = "contact-info";
+
+    street.classList.add('contact-street');
+    city.classList.add('contact-city');
+    phone.classList.add('contact-phone');
+    email.classList.add('contact-email');
+
+    street.textContent = "1600 Pennsylvania Ave,";
+    city.textContent = "Washington DC";
+    phone.textContent = "123-555-6789";
+    email.textContent = "customerservice@brunchfast.org";
+
+    contactContainer.appendChild(street);
+    contactContainer.appendChild(city);
+    contactContainer.appendChild(phone);
+    contactContainer.appendChild(email);
+
+    return contactContainer
 }
 
 function loadMain(){
